25f9b8af353ac25a08c50f102fccd9d16c0df18a
yuvanselvm <yuvanselvam626117@gmail.com>
2022-06-21 20:31:02 +0530

    Day 1 Added add_game_btn and organized widgets.py

--- widgets.py
+++ widgets.py
@@ -1,22 +1,40 @@
-from PySide6.QtWidgets import QWidget
+from turtle import shape
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PySide6.QtGui import QCursor 
 
 
 class HomeWidget(QWidget):
     def __init__(self):
         """ Main Widgets Class """
         super().__init__()
+        self.setupScr()
+        self.setupLayout()
+        self.setupWidgets()
 
-        # CONSTANTS
+    def setupScr(self):
+        """ Screen Setup """
+        self.setWindowTitle('Game Manager')
+        self.scr = self.screen().size()
+
+        # calculating window pos ( centering the window - dividing height and width by 4 works)
+        self.window_pos = (self.scr.width()/4, self.scr.height()/4)
+
         self.HEIGHT = 650
         self.WIDTH = 450
 
-        self.setup()
-
-    def setup(self):
-        """ Screen Setup """
-        self.setWindowTitle('Game Manager')
-        # screen size
-        self.scr = self.screen().size() 
-        # calculating window pos ( centering the window - dividing height and width by 4 works)
-        self.window_pos = (self.scr.width()/4, self.scr.height()/4) 
         self.setGeometry(*self.window_pos, self.HEIGHT, self.WIDTH)
+
+    def setupLayout(self):
+        """ Initilize a Vertical Layout """
+        self.vlayout = QVBoxLayout(self)
+        self.setLayout(self.vlayout)
+
+    def setupWidgets(self):
+        """ Makes and Adds Widgets to the vlayout """
+        # Add Game Btn
+        self.add_game_btn = QPushButton('Add Game')
+        self.pointing_cursor = QCursor().shape().PointingHandCursor
+        self.add_game_btn.setCursor(self.pointing_cursor)
+
+        # Add Widgets to vlayout
+        self.vlayout.addWidget(self.add_game_btn)
